2aee3436c86f85879042041c0ea96e8cabde89d1
Scott T Lyman <s.t.lyman84@gmail.com>
2022-03-18 15:18:50 -0600

    init document

---
+++ tower.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'dev-0qbemmfr.us.auth0.com'
+export const clientId = 'okzxlylwRVEsDEvXOKfL2e4eB0r5O6XU'
+export const audience = 'https://scott-dev.com'
